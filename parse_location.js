b7823f848562a380ee26ecc0929844ffbcf1d99d
Jason Hessley <jason.sunbelt@gmail.com>
2024-08-15 08:44:01 -0400

    locationID

--- parse_location.js
+++ parse_location.js
@@ -14,8 +14,7 @@ var link = document.createElement('link');
 // Set the attributes for the link element
 link.rel = 'stylesheet';
 link.type = 'text/css';
-link.href = 'https://example.com/style.css'; // URL of the external CSS file
+link.href = 'https://novelcrm.github.io/app/' + locationID + '.css'; // URL of the external CSS file
 
 // Append the link element to the head
 document.getElementsByTagName('head')[0].appendChild(link);
-var t = "just a test."
